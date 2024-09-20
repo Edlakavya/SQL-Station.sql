4a51ab14407f7ec0fc1fa03eb09bf29c25e66310
Edlakavya <70639824+Edlakavya@users.noreply.github.com>
2024-09-20 15:45:48 +0530

    Update HackerRank.sql

--- HackerRank.sql
+++ HackerRank.sql
@@ -25,3 +25,43 @@ where substr(city,1,1) not in ('A','E','I','O','U') or right(city,1) not in ('a'
 -- 7)Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
 select distinct(city) from station
 where substr(city,1,1) not in ('A','E','I','O','U') and right(city,1) not in ('a','e','i','o','u');
+
+-- 8) Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+select name from students
+where marks>75 order by right(name,3), id;
+
+-- 9) Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+select name from employee
+order by name;
+
+
+-- Write a query that prints a list of employee names (i.e.: the name attribute) for employees in Employee having a salary greater than  per month who have been employees for less than  months. Sort your result by ascending employee_id.
+select name from employee
+where salary>2000 and months<10 order by employee_id;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
